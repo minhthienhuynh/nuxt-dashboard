25f10a7363b1ebd22766f15920b505d77fb9df2b
Thien <thien.huynhminh@mfast.vn>
2026-05-24 10:49:44 +0700

    feat: add rebuild API endpoint for services

---
+++ server/api/services/[id]/rebuild.post.ts
@@ -0,0 +1,17 @@
+import { getRouterParam } from 'h3'
+import { ServiceService } from '~~/server/services/service.service'
+import { DockerService } from '~~/server/services/docker.service'
+import { serviceIdSchema } from '~~/server/validators/service.schema'
+import { handleError } from '~~/server/utils/errors'
+
+export default eventHandler(async (event) => {
+  try {
+    const id = serviceIdSchema.parse(getRouterParam(event, 'id'))
+    const service = await ServiceService.getById(id)
+    await DockerService.rebuildService(service as unknown as import('~/types').InfrastructureService)
+    const status = await DockerService.getContainerStatus(service.containerName)
+    return { status, containerName: service.containerName }
+  } catch (error) {
+    throw handleError(error)
+  }
+})
